cdbe64c8b7f629a1fdac75a44df092531eb62058
Codebreaker <37763424+FatimaBasheer@users.noreply.github.com>
2020-05-17 16:46:17 +0530

    Update code

--- doc_classification_ml.py
+++ doc_classification_ml.py
@@ -4,15 +4,25 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.sparse import hstack
-from matplotlib import pyplot as plt
 import seaborn as sns
 import eli5
 from IPython.display import Image
-
 import os
-for dirname, _, filenames in os.walk('./Datasets/bbc/'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import chi2
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+
+
+# for dirname, _, filenames in os.walk('./Datasets/bbc/'):
+#     for filename in filenames:
+#         print(os.path.join(dirname, filename))
 
 # Any results you write to the current directory are saved as output.
 # Step 1 - Get the file details
@@ -23,10 +33,6 @@ text = []
 label = []
 datapath = './Datasets/bbc/' 
 for dirname, _ , filenames in os.walk(datapath):
-    #print('Directory: ', dirname)
-    #print('Subdir: ', dirname.split('/')[-1])
-    # remove the Readme.txt file
-    # will not find file in the second iteration so we skip the error
     try:
         filenames.remove('README.TXT')
     except:
@@ -49,3 +55,156 @@ for dirname, _ , filenames in os.walk(datapath):
             text.append(intext)
 
 #creating df frames for text analysis
+fulldf = pd.DataFrame(list(zip(directory, file, title, text, label)), 
+               columns =['directory', 'file', 'title', 'text', 'label'])
+
+df = fulldf.filter(['title','text','label'], axis=1)
+
+# print("FullDf : ", fulldf.shape)
+# print("DF : ", df.shape)
+
+df['category_id'] = df['label'].factorize()[0]
+category_id_df = df[['label', 'category_id']].drop_duplicates().sort_values('category_id')
+category_to_id = dict(category_id_df.values)
+id_to_category = dict(category_id_df[['category_id', 'label']].values)
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english')
+features = tfidf.fit_transform(df.text).toarray()
+labels = df.category_id
+
+#feature selection for the given text files
+N = 3
+for label, category_id in sorted(category_to_id.items()):
+  features_chi2 = chi2(features, labels == category_id)
+  indices = np.argsort(features_chi2[0])
+  feature_names = np.array(tfidf.get_feature_names())[indices]
+  unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
+  bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
+#   print("# '{}':".format(label))
+#   print("  . Most correlated unigrams:\n       . {}".format('\n       . '.join(unigrams[-N:])))
+#   print("  . Most correlated bigrams:\n       . {}".format('\n       . '.join(bigrams[-N:])))
+
+
+#tfidf vector for each article
+# Sampling a subset of our dataset because t-SNE is computationally expensive
+SAMPLE_SIZE = int(len(features) * 0.3)
+np.random.seed(0)
+indices = np.random.choice(range(len(features)), size=SAMPLE_SIZE, replace=False)
+projected_features = TSNE(n_components=2, random_state=0).fit_transform(features[indices])
+colors = ['pink', 'green', 'midnightblue', 'orange', 'darkgrey']
+for category, category_id in sorted(category_to_id.items()):
+    points = projected_features[(labels[indices] == category_id).values]
+    plt.scatter(points[:, 0], points[:, 1], s=30, c=colors[category_id], label=category)
+plt.title("tf-idf feature vector for each article, projected on 2 dimensions.",
+          fontdict=dict(fontsize=15))
+plt.legend()
+
+
+#cv for diff ML models
+models = [
+    RandomForestClassifier(n_estimators=200, max_depth=3, random_state=0),
+    MultinomialNB(),
+    LogisticRegression(random_state=0),
+    SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter_no_change=5, random_state=42)
+]
+CV = 5
+cv_df = pd.DataFrame(index=range(CV * len(models)))
+entries = []
+for model in models:
+  model_name = model.__class__.__name__
+  accuracies = cross_val_score(model, features, labels, scoring='accuracy', cv=CV)
+  for fold_idx, accuracy in enumerate(accuracies):
+    entries.append((model_name, fold_idx, accuracy))
+cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_idx', 'accuracy'])
+
+#seaplot
+sns.boxplot(x='model_name', y='accuracy', data=cv_df)
+sns.stripplot(x='model_name', y='accuracy', data=cv_df, 
+              size=8, jitter=True, edgecolor="gray", linewidth=2)
+cv_df.groupby('model_name').accuracy.mean()
+
+#confusion matrices for different models
+X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
+#Logistic Regression
+lrmodel = LogisticRegression(random_state=0)
+y_pred_proba = lrmodel.predict_proba(X_test)
+y_pred = lrmodel.predict(X_test)
+
+conf_mat = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_mat, annot=True, fmt='d',
+            xticklabels=category_id_df.label.values, yticklabels=category_id_df.label.values)
+plt.ylabel('LR Actual')
+plt.xlabel('LR Predicted')
+#SVM
+svmmodel = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, max_iter=5, random_state=42)
+clf = svmmodel.fit(X_train, y_train)
+calibrator = CalibratedClassifierCV(clf, cv='prefit')
+svmmodel=calibrator.fit(X_train, y_train)
+
+
+y_pred_proba = svmmodel.predict_proba(X_test)
+y_pred = svmmodel.predict(X_test)
+
+conf_mat = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_mat, annot=True, fmt='d',
+            xticklabels=category_id_df.label.values, yticklabels=category_id_df.label.values)
+plt.ylabel('SVM Actual')
+plt.xlabel('SVM Predicted')
+#Random Forest Classifier
+rfcmodel = RandomForestClassifier(n_estimators=200, max_depth=3, random_state=0)
+y_pred_proba = rfcmodel.predict_proba(X_test)
+y_pred = rfcmodel.predict(X_test)
+
+conf_mat = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_mat, annot=True, fmt='d',
+            xticklabels=category_id_df.label.values, yticklabels=category_id_df.label.values)
+plt.ylabel('RFC Actual')
+plt.xlabel('RFC Predicted')
+#Naive Bayes
+nbmodel = MultinomialNB()
+y_pred_proba = nbmodel.predict_proba(X_test)
+y_pred = nbmodel.predict(X_test)
+
+conf_mat = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_mat, annot=True, fmt='d',
+            xticklabels=category_id_df.label.values, yticklabels=category_id_df.label.values)
+plt.ylabel('NB Actual')
+plt.xlabel('NB Predicted')
+
+# #display mismatches
+# for predicted in category_id_df.category_id:
+#   for actual in category_id_df.category_id:
+#     if predicted != actual and conf_mat[actual, predicted] >= 2:
+#       print("'{}' predicted as '{}' : {} examples.".format(id_to_category[actual], id_to_category[predicted], conf_mat[actual, predicted]))
+#       display(df.loc[indices_test[(y_test == actual) & (y_pred == predicted)]][['label', 'text']])
+#       print('')
+
+#Predict using Logistic Regression
+lrmodel.fit(features, labels)
+
+N = 5
+for category, category_id in sorted(category_to_id.items()):
+  indices = np.argsort(model.coef_[category_id])
+  feature_names = np.array(tfidf.get_feature_names())[indices]
+  unigrams = [v for v in reversed(feature_names) if len(v.split(' ')) == 1][:N]
+  bigrams = [v for v in reversed(feature_names) if len(v.split(' ')) == 2][:N]
+  print("# '{}':".format(category))
+  print("  . Top unigrams:\n       . {}".format('\n       . '.join(unigrams)))
+  print("  . Top bigrams:\n       . {}".format('\n       . '.join(bigrams)))
+
+# df[df.text.str.lower().str.contains('news website')].label.value_counts()
+#Prediction
+texts = ["Hooli stock price soared after a dip in PiedPiper revenue growth.",
+         "Captain Tsubasa scores a magnificent goal for the Japanese team.",
+         "Merryweather mercenaries are sent on another mission, as government oversight groups call for new sanctions.",
+         "Beyoncé releases a new album, tops the charts in all of south-east Asia!",
+         "You won't guess what the latest trend in data analysis is!"]
+text_features = tfidf.transform(texts)
+predictions = lrmodel.predict(text_features)
+for text, predicted in zip(texts, predictions):
+  print('"{}"'.format(text))
+  print("  - Predicted as: '{}'".format(id_to_category[predicted]))
+  print("")
+
